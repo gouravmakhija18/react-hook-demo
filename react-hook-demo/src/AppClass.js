8af38ecdcfec69d5e25347d8eb6e05c3d9333f84
Gourav Mukhija <gmukhija@sapient.com>
2020-04-20 00:54:09 -0400

    React hook : counter + flashlight changes

--- react-hook-demo/src/AppClass.js
+++ react-hook-demo/src/AppClass.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 
 class App extends Component{
   state = {
-    count: 0
+    count: 0,
+    isOn: true
   }
   /*
   * Enhancement : previousState used so that on each refresh we get correct state object
@@ -12,9 +13,29 @@ class App extends Component{
       count: previousState.count+1
     }))
   }
+
+  onImageToggle = () => {
+    this.setState(prevIsOn => ({
+      isOn :!prevIsOn.isOn
+    }))
+  };
   render(){
     return (
-      <button onClick = {this.incrementCount}> Button click count {this.state.count}</button>
+      <>
+        <h2>Couter</h2>
+        <button onClick = {this.incrementCount}> Button click count {this.state.count}</button>
+        <h2> Flash Light Toggle</h2>
+        <div
+          style= {{
+            width: "50px",
+            height: "50px",
+            background: this.state.isOn ? "green" : "red"
+          }}
+
+          onClick = {this.onImageToggle}
+        >
+        </div>
+      </>
     );
   }
 }
--- react-hook-demo/src/AppFunction.js
+++ react-hook-demo/src/AppFunction.js
@@ -3,6 +3,7 @@ import React, {useState} from 'react';
 function App(){
 
   const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(true);
 
   /*
   * Note here count work as this.state.count & setCount as this.setState({count})
@@ -11,8 +12,27 @@ function App(){
   const incrementCount = () => {
     setCount(prevCount => prevCount+1);
   }
+
+  const onImageToggle = () => {
+    setIsOn(prevIsOn => !prevIsOn);
+  }
+
   return (
-    <button onClick = {incrementCount}> Button click count {count}</button>
+    <>
+      <h2>Couter</h2>
+      <button onClick = {incrementCount}> Button click count {count}</button>
+      <h2> Flash Light Toggle</h2>
+      <div
+        style= {{
+          width: "50px",
+          height: "50px",
+          background: isOn ? "green" : "red"
+        }}
+
+        onClick = {onImageToggle}
+      >
+      </div>
+    </>
   );
 }
 
